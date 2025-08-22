0920bcce7036d8422a22c0d47c6aa10f5ab7ecac
tahahhany11192 <tahah8833@gmail.com>
2025-08-22 03:25:48 +0300

    Update instructors.js

--- Scripts/instructors.js
+++ Scripts/instructors.js
@@ -3,7 +3,7 @@ const form = document.getElementById('instructor-form');
 const modal = document.getElementById('instructor-form-modal');
 
 function fetchInstructors() {
-  fetch('https://backend-g8fsuq.fly.devapi/instructors/getinstructor')
+  fetch('https://orycom-backend.fly.devapi/instructors/getinstructor')
     .then(res => res.json())
     .then(data => {
       container.innerHTML = '';
@@ -66,13 +66,13 @@ form.onsubmit = async (e) => {
   if (!id) {
     formData.append('password', document.getElementById('password').value);
 
-    await fetch('https://backend-g8fsuq.fly.devapi/instructors/addinstructor', {
+    await fetch('https://orycom-backend.fly.devapi/instructors/addinstructor', {
       method: 'POST',
       body: formData
       
     });
   } else {
-    await fetch(`https://backend-g8fsuq.fly.devapi/instructors/${id}`, {
+    await fetch(`https://orycom-backend.fly.devapi/instructors/${id}`, {
       method: 'PUT',
       body: formData
     });
